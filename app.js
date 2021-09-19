3953a6f05fd9038705127805db72aea6202f2d7f
segundov1235 <segundovallejos1235@gmail.com>
2021-09-19 05:37:54 -0300

    cree js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ alert("hola!")
  console.log(`${nombre} ${apellido} ${edad}`)
 
  let numero1 = Number(prompt("ingrese un numero"))
- let numero2 = Number(prompt("ingrese otr numero"))
+ let numero2 = Number(prompt("ingrese otro numero"))
 
  console.log(numero1 + numero2)
  console.log(numero1 - numero2)
